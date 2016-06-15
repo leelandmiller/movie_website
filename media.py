f65e63cd07046d8e22bb8694ee7e0ddea278151c
leelandmiller <leelandmiller@gmail.com>
2016-06-15 15:58:42 -0700

    Code revisions

--- media.py
+++ media.py
@@ -5,12 +5,13 @@ class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     #constructor
     def __init__(self, title, storyline, poster_image_url,
-                 trailer_youtube_url):
+                 trailer_youtube_url, rating):
         #instance variables
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.rating = rating
 
     #end __init__ method
 
